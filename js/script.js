cac3c7879d09e735400c53d821bd6d044b89ab3d
aroyz <90589816+aroyz@users.noreply.github.com>
2021-09-23 14:48:39 -0400

    More animation fiddling

--- js/script.js
+++ js/script.js
@@ -3,13 +3,19 @@
 
 $(document).ready(function() {
 	$("a#vprospect").click(function() {
-		$("#solution").load("prospect.html #vprospect").hide().slideDown(1000).css("height", "401px");
+		$("#solution").load("prospect.html #vprospect").slideDown(1000, function(){
+			$(this).css("height", "401px");
+		});
 	});
 	$("a#vconvert").click(function() {
-		$("#solution").load("convert.html #vconvert").hide().slideDown(1000).css("height", "601px");
+		$("#solution").load("convert.html #vconvert").slideDown(1000, function(){
+			$(this).css("height", "601px");
+		});
 	});
 	$("a#vretain").click(function() {
-		$("#solution").load("retain.html #vretain").hide().slideDown(1000).css("height", "351px");
+		$("#solution").load("retain.html #vretain").slideDown(1000, function(){
+			$(this).css("height", "351px");
+		});
 	});
 });
 
