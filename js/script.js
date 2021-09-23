23968975e1d6c0c1c9af34632541f525ea5643ca
aroyz <90589816+aroyz@users.noreply.github.com>
2021-09-23 14:14:49 -0400

    Added slideDown animation

--- js/script.js
+++ js/script.js
@@ -3,13 +3,13 @@
 
 $(document).ready(function() {
 	$("a#vprospect").click(function() {
-		$("#solution").load("prospect.html #vprospect");
+		$("#solution").load("prospect.html #vprospect").hide().slideDown(1000);
 	});
 	$("a#vconvert").click(function() {
-		$("#solution").load("convert.html #vconvert");
+		$("#solution").load("convert.html #vconvert").hide().slideDown(1000);
 	});
 	$("a#vretain").click(function() {
-		$("#solution").load("retain.html #vretain");
+		$("#solution").load("retain.html #vretain").slideDown(1000);
 	});
 });
 
