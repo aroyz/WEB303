5325248edf0f9bd9dc32b632711ad9f1c6f2c53e
aroyz <90589816+aroyz@users.noreply.github.com>
2021-09-24 13:33:53 -0400

    Animation Fiddling 2: Electric Boogaloo

--- js/script.js
+++ js/script.js
@@ -3,19 +3,19 @@
 
 $(document).ready(function() {
 	$("a#vprospect").click(function() {
-		$("#solution").load("prospect.html #vprospect").slideDown(1000, function(){
-			$(this).css("height", "401px");
-		});
+		$("#solution").load("prospect.html #vprospect", function() {
+			$(this).css("height", "401px").hide();
+		}).slideDown(1000);
 	});
 	$("a#vconvert").click(function() {
-		$("#solution").load("convert.html #vconvert").slideDown(1000, function(){
-			$(this).css("height", "601px");
-		});
+		$("#solution").load("convert.html #vconvert", function() {
+			$(this).css("height", "601px").hide();
+		}).slideDown(1000);
 	});
 	$("a#vretain").click(function() {
-		$("#solution").load("retain.html #vretain").slideDown(1000, function(){
-			$(this).css("height", "351px");
-		});
+		$("#solution").load("retain.html #vretain", function() {
+			$(this).css("height", "351px").hide();
+		}).slideDown(1000);
 	});
 });
 
