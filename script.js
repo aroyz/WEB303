a689585ee7540b5bb0c9c26e94ceeb48d7889b96
aroyz <90589816+aroyz@users.noreply.github.com>
2021-09-30 14:30:06 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// WEB303 Assignment 3
+// Abbey Roy
+
+$(document).ready(function() {
+	const jsonFunction = function() {
+		$.getJSON("team.json", function(team) {
+			$.each(team.teammembers, function(index, teamMember) {
+				$("div#team").append(
+					`<div id="member${index}">
+						<h3>${teamMember.name}</h3>
+						<h4>${teamMember.title}</h4>
+						<p>${teamMember.bio}</p>
+					</div>`
+				);
+			});
+		});
+	}
+	const ajaxFunction = function() {
+		
+	}
+	jsonFunction();
+});
