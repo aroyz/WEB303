d299dc8107e1e3cb0e7ee889068339fc63ca3618
aroyz <90589816+aroyz@users.noreply.github.com>
2021-10-04 20:55:17 -0400

    Completed Assignment 3

--- script.js
+++ script.js
@@ -2,27 +2,45 @@
 // Abbey Roy
 
 $(document).ready(function() {
+	const dataProcessing = function(team) {
+		$.each(team.teammembers, function(index, teamMember) {
+			$("div#team").append(
+				`<div id="member${index}">
+					<h3>${teamMember.name}</h3>
+					<h4>${teamMember.title}</h4>
+					<p>${teamMember.bio}</p>
+				</div>`
+			);
+		});
+	}
 	const jsonFunction = function() {
 		$.getJSON("team.json", function(team) {
-			$.each(team.teammembers, function(index, teamMember) {
-				$("div#team").append(
-					`<div id="member${index}">
-						<h3>${teamMember.name}</h3>
-						<h4>${teamMember.title}</h4>
-						<p>${teamMember.bio}</p>
-					</div>`
-				);
-			});
+			dataProcessing(team);
 		});
 	}
 	const ajaxFunction = function() {
-		let xhr = new XMLHttpRequest();
-		xhr.open("GET", "team.json", true);
-		xhr.onreadystatechange = function() {
-			let team = JSON.parse(xhr.responseText);
-			console.log(team);
-		}
-		xhr.send();
+		$.ajax({
+			url: 'team.json',
+			type: 'GET',
+			dataType: 'json',
+			beforeSend: function() {
+				$("div#team").html(
+					`<p>Loading...</p>`
+				);
+			},
+			error: function(xhr) {
+				$("div#team").html(
+					`<p>Data failed to load.</p>`
+				);
+				alert(`An error ocured while retrieving data. Error code ${xhr.status}: ${xhr.statusText}`);
+			},
+			timeout: 5000,
+			success: function(team) {
+				$("div#team").empty();
+				dataProcessing(team);
+			}
+		});
 	}
+	//jsonFunction();
 	ajaxFunction();
 });
