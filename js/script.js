2e5d99360d0f0da64c23e8c19ab130b99b42628c
aroyz <90589816+aroyz@users.noreply.github.com>
2021-11-11 12:03:35 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,28 @@
-$('document').ready(function() {
+$(document).ready(function() {
+    let $accordions = $('.accordion-wrapper');
+    let $headers = $('#accordion-wrapper-1 .panel-header');
 
+    $accordions.each(function () {
+        let $currAccordion = $(this).attr('id');
+        let $headers = $('#' + $currAccordion).find('.panel-header');
+
+        $headers.each(function (index) {
+            $(this).on('click', function (event) {
+                event.preventDefault();
+
+                if ($(this).next().hasClass('active')) {
+                    $(this).next().removeClass('active');
+                    $(this).css('border-bottom', '0px');
+                }
+                else {
+                    $headers.each(function () {
+                        $(this).next().removeClass('active');
+                        $(this).css('border-bottom', '0px');
+                    });
+                    $(this).next().addClass('active');
+                    $(this).css('border-bottom', '1px solid black');
+                }
+            });
+        });
+    });
 });
